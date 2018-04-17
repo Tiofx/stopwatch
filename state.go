282abea2af830d27378b6c4a31dfe6f920feff60
Tiofx <ioracle2718@gmail.com>
2018-04-17 21:13:04 +0300

    [add] docs for state

--- state.go
+++ state.go
@@ -2,12 +2,32 @@ package stopwatch
 
 import "fmt"
 
+// state represent Stopwatch's current state.
 type state int
 
+// HasSplitTime check presents of splitTime state.
 func (s state) HasSplitTime() bool { return s&splitTime != 0 }
 
+// WithoutSplitTime returning state without splitTime.
+// Its let check main state without bitwise operation:
+//
+// Examples for check on Initial state:
+// Incorrect if state has splitTime:
+// 		sw.state() == Initial
+//
+// Correct but almost unreadable:
+// 		(sw.state() | splitTime) ^ splitTime == Initial
+//
+// Perfect:
+//		sw.state().WithoutSplitTime() == Initial
 func (s state) WithoutSplitTime() state { return (s | splitTime) ^ splitTime }
 
+// WithSplitTime add splitTime state.
+// If state already has splitTime than nothing changes.
+func (s state) WithSplitTime() state { return s | splitTime }
+
+// Set change receiver to `to` state without
+// changing splitTime of receiver.
 func (s *state) Set(to state) {
 	if s.HasSplitTime() {
 		*s = to.WithSplitTime()
@@ -16,6 +36,24 @@ func (s *state) Set(to state) {
 	}
 }
 
+// Stopwatch can be in several states:
+// Initial, Running, Stopped.
+// Additionally to Running or Stooped states
+// Stopwatch can has the splitTime state.
+//
+// Initial - initial state that has Stopwatch right after creation or
+// after reset.
+//
+// Running - state in which Stopwatch's displayed elapsed time
+// change over time.
+//
+// Stopped - state in which Stopwatch Display the same time over time.
+//
+//
+// splitTime - when Stopwatch get this state its displayed time will be frozen
+// but elapsed time can be still increasing.
+// When Stopwatch get out from this state
+// its displayed time will be updated and unfrozen.
 const (
 	Initial state = iota
 	Running
