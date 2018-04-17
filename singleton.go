8c6bf008b5b1d38329d513c5688607dab7284071
Tiofx <ioracle2718@gmail.com>
2018-04-17 21:17:59 +0300

    [add] stopwatch singleton

---
+++ singleton.go
@@ -0,0 +1,16 @@
+package stopwatch
+
+import "sync"
+
+var instance *Stopwatch
+var once sync.Once
+
+// Global, single, thread-save instance of Stopwatch.
+// It can be used in situation when the same Stopwatch
+// needed in a different places and passing around an instance
+// is inconvenient.
+func Global() *Stopwatch {
+	once.Do(func() { instance = New() })
+	
+	return instance
+}
