73f183dfe4c54f92959f8749ff53cec042280d33
Tiofx <ioracle2718@gmail.com>
2018-04-17 21:19:15 +0300

    [add] benchmark
    for fun

---
+++ benchmark_test.go
@@ -0,0 +1,42 @@
+package stopwatch
+
+import (
+	"testing"
+	"stopwatch"
+)
+
+var (
+	resSw  *stopwatch.Stopwatch
+	resStr string
+)
+
+func BenchmarkNew(b *testing.B) {
+	var sw *stopwatch.Stopwatch
+
+	for i := 0; i < b.N; i++ {
+		sw = stopwatch.New()
+	}
+
+	resSw = sw
+
+	b.ReportAllocs()
+}
+
+func BenchmarkSimple(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		sw := stopwatch.New()
+		sw.PressTopButton()
+		sw.PressTopButton()
+		sw.PressSecondButton()
+	}
+}
+
+func BenchmarkStopwatch_String(b *testing.B) {
+	var string string
+
+	for i := 0; i < b.N; i++ {
+		string = stopwatch.Global().String()
+	}
+
+	resStr = string
+}
