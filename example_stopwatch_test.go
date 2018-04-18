4936f23f2c29b484a5f5df1703e9158e384d50de
Tiofx <ioracle2718@gmail.com>
2018-04-18 23:09:29 +0300

    [change] return version of stopwatch examples

--- example_stopwatch_test.go
+++ example_stopwatch_test.go
@@ -1,18 +1,19 @@
-package stopwatch
+package stopwatch_test
 
 import (
 	"fmt"
+	"github.com/Tiofx/stopwatch"
 	"time"
 )
 
 func ExampleStopwatch_Display_first() {
-	fmt.Println(New().Display())
+	fmt.Println(stopwatch.New().Display())
 	//	Output: 0s
 }
 
 // Show method invocation time.
 func ExampleStopwatch_Display_second() {
-	s := New()
+	s := stopwatch.New()
 
 	s.PressTopButton()
 	s.PressTopButton()
@@ -22,52 +23,54 @@ func ExampleStopwatch_Display_second() {
 
 func ExampleStopwatch_Display_third() {
 	delay := 10 * time.Millisecond
-	s := New()
+	s := stopwatch.New()
 
 	s.PressTopButton()
 	time.Sleep(delay)
 	s.PressTopButton()
 
+	fmt.Println(isEqual(s.Display(), delay))
+	//	Output: true
 }
 
 func ExampleStopwatch_State() {
-	fmt.Println(New().State() == Initial)
+	fmt.Println(stopwatch.New().State() == stopwatch.Initial)
 	//	Output: true
 }
 
 func ExampleStopwatch_State_first() {
-	s := New()
+	s := stopwatch.New()
 	s.PressTopButton()
 
-	fmt.Println(s.State() == Running)
+	fmt.Println(s.State() == stopwatch.Running)
 	//	Output: true
 }
 
 func ExampleStopwatch_State_secondA() {
-	s := New()
+	s := stopwatch.New()
 	s.PressTopButton()
 	s.PressSecondButton()
 
-	fmt.Println(s.State() == Running)
+	fmt.Println(s.State() == stopwatch.Running)
 	//	Output: false
 }
 
 func ExampleStopwatch_State_secondB() {
-	s := New()
+	s := stopwatch.New()
 	s.PressTopButton()
 	s.PressSecondButton()
 
 	fmt.Println(
-		s.State() == Running.WithSplitTime() &&
+		s.State() == stopwatch.Running.WithSplitTime() &&
 			s.State().HasSplitTime() &&
-			s.State().WithoutSplitTime() == Running,
+			s.State().WithoutSplitTime() == stopwatch.Running,
 	)
 	//	Output: true
 }
 
 //Show example of representation Stopwatch as string
 func ExampleStopwatch_String() {
-	s := New()
+	s := stopwatch.New()
 	s.PressTopButton()
 	time.Sleep(1 * time.Millisecond)
 	s.PressTopButton()
